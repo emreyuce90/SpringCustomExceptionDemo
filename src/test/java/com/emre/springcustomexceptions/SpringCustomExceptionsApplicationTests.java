0acd962e5e202dc7433c5f558365ce19726ff4ef
Emre Yüce <emre.yuce@simetri8.com>
2025-02-07 00:05:29 +0300

    initial commit

---
+++ src/test/java/com/emre/springcustomexceptions/SpringCustomExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emre.springcustomexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
